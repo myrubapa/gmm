561ae3b4b5eb1eb0e3ce11e130c8307b258afbff
jimmythorn <jthorn@centresource.com>
2015-06-02 00:51:51 -0500

    Built out hero, program, and program-preview sections

---
+++ app/scripts/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#hero-carousel").owlCarousel({
+      navigation : true,
+      slideSpeed : 450,
+      paginationSpeed : 400,
+      mouseDrag : false,
+      autoHeight : true,
+      singleItem:true
+  });
+});
