039007e98cee5d816d122c683eb6e8b42b5b8c3f
jimmythorn <jthorn@centresource.com>
2015-07-19 09:40:32 -0500

    Added PZI page and sections to the repo and broke the scroll detect js into a separate file

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 'use strict';
 
-  $('#hero-carousel').owlCarousel({
+  $('.owl-carousel').owlCarousel({
       autoplay : true,
       autoplayHoverPause : true,
       smartSpeed : 650,
@@ -14,60 +14,4 @@ $(document).ready(function() {
   });
 
   $('.scroll-link').smoothScroll();
-
-
-  var delta, didScroll, hasScrolled, lastScrollTop, navbarHeight;
-
-  didScroll = void 0;
-
-  lastScrollTop = 0;
-
-  delta = 5;
-
-  navbarHeight = $('#sneak-peek').outerHeight();
-
-  hasScrolled = function() {
-    var st;
-    st = $(this).scrollTop();
-    if (Math.abs(lastScrollTop - st) <= delta) {
-      return;
-    }
-    if (st > lastScrollTop && st > navbarHeight) {
-      $('#sneak-peek').removeClass('peek-up').addClass('peek-down');
-    } else {
-      if (st + $(window).height() < $(document).height()) {
-        $('#sneak-peek').removeClass('peek-down').addClass('peek-up');
-      }
-    }
-    lastScrollTop = st;
-  };
-
-  $(window).scroll(function() {
-    didScroll = true;
-  });
-
-  setInterval((function() {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }), 250);
-
-
-  $(window).scroll(function() {
-     var hT = $('#do-good').offset().top,
-         hH = $('#do-good').outerHeight(),
-         wH = $(window).height(),
-         wS = $(this).scrollTop();
-     if (wS > (hT+hH-wH)){
-       $('#sneak-peek').addClass('hide');
-       $('body').addClass('hide');
-     }
-     else if (wS < (hT+hH-wH)) {
-      $('#sneak-peek').removeClass('hide');
-      $('body').removeClass('hide');
-     }
-  });
-
-
 });
